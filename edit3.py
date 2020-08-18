3dac87caddced6084f9904d50dadbd2f80e827b2
NN003 <52203174+nm33@users.noreply.github.com>
2020-08-18 12:44:14 +0700

    Add files via upload

---
+++ edit3.py
@@ -0,0 +1,503 @@
+import os, sys, time
+from time import sleep as timeout
+def restart_program():
+        python = sys.executable
+        os.execl(python, python, * sys.argv)
+        curdir = os.getcwd()
+os.system("clear")
+os.system("figlet EDIT BOT V3.3")
+print "CREATOR  : NN SELFBOT"
+print "TEAMBOT  : AVENGERS "
+print "Version  : 3.3                "
+print
+print "          []============================ "
+print "          [] <[Command Set Edit]>     []                 "
+print "          []============================ "
+print "          [] 1 > SELF BOT  "
+print "          [] 2 > JS BOT   "
+print "          [] 3 > PROTECT BOT  "
+print "          [] 4 > PACKAGE BOT  "
+print "          [] 5 > UPDATE SYSTEM  "
+print "          [] 6 > INSTALL PACKAGE NOW  "
+print "          [] 7 > HOW TO USE    "
+print "          [] 8 > CONTACT THE CREATOR  "
+print "    []===== "
+print "    []        "
+print "    [] 001 > INSTALL FREE BOT NOW "
+print "    [] 002 > Show Login Point "
+print "    [] 003 > Information Edit "
+print "    [] os >  Token Os Bot"
+print "    [] nn >  Don't print (command creator) "
+print "    [] bot > Login free bot "
+print "    []  "
+print "    [] 00 > Exit Program"
+print "    []============================"
+print
+print
+A = raw_input("[Enter Command] >> ")
+if A == "1" or A == "01":
+  os.system("figlet Loading")
+  time.sleep(2)
+  os.system("figlet Login Now")
+  time.sleep(1)
+  os.system("cd 3000 && python3 moo.py")
+  
+elif A == "2" or A == "02":
+  os.system("figlet Loading")
+  time.sleep(2)
+  os.system("figlet Login Now")
+  time.sleep(1)
+  os.system("cd && cd bin && npm start")
+      
+elif A == "3" or A == "03":
+  os.system("figlet Loading")
+  time.sleep(2)
+  os.system("figlet Login Now")
+  time.sleep(1)
+  os.system("cd 3000 && python3 mon1.h")
+    
+elif A == "4" or A == "04":
+    os.system("figlet INSTALL NOW")
+    print "package install python"
+    print
+    print "Wait 10 seconds"
+    time.sleep(10)
+    os.system("clear")
+    print
+    print
+    print
+    print "          apt install python -y"
+    print "          apt install git -y"
+    print "          apt install nodejs -y"
+    print "          apt install nano -y"
+    print "          pip install --upgrade pip"
+    print "          apt update && apt upgrade -y"
+    print "          pip3 install six"
+    print "          pip3 install rsa"
+    print "          pip3 install requests"
+    print "          pip3 install googletrans"
+    print "          pip3 install thrift"
+    print "          pip3 install bs4"
+    print "          pip3 install html5lib"
+    print "          pip3 install pafy"
+    print "          pip3 install pytz"
+    print "          pip3 install humanfriendly"
+    print "          pip3 install humanize"
+    print "          pip3 install gtts"
+    print "          pip3 install wikipedia"
+    print "          pip3 install youtube_dl"
+    print "          pip3 install antolib"
+    print
+    print
+    os.system("figlet TIME OUT")
+    os.system("figlet  30 seconds")
+    time.sleep(30)
+    restart_program()
+    
+elif A == "5" or A == "05":
+    os.system("figlet Loading")
+    time.sleep(1)
+    os.system("figlet Update")
+    time.sleep(1)
+    os.system("figlet Now")
+    time.sleep(2)
+    os.system("toilet -f mono12 -F gay N OS")
+    os.system("apt update && apt upgrade -y")  
+    time.sleep(1)
+    os.system("figlet Success")
+    time.sleep(3)
+    restart_program()
+    
+elif A == "6" or A == "06":
+    os.system("figlet Loading")
+    time.sleep(2)
+    os.system("apt update && apt upgrade -y")
+    time.sleep(1)
+    os.system("apt install python -y")
+    time.sleep(1)
+    os.system("apt install git -y")
+    time.sleep(1)
+    os.system("apt install nodejs -y")
+    time.sleep(1)
+    os.system("apt install toilet")
+    time.sleep(1)
+    os.system("apt install neofetch")
+    time.sleep(1)
+    os.system("apt install nano -y")
+    time.sleep(1)  
+    os.system("apt install unzip")
+    time.sleep(1)
+    os.system("apt install figlet")
+    time.sleep(1)
+    os.system("pip install --upgrade pip")
+    time.sleep(1)
+    os.system("pip3 install six")
+    time.sleep(1)
+    os.system("pip3 install rsa")
+    time.sleep(1)
+    os.system("apt install cowsay")
+    time.sleep(1)
+    os.system("pip3 install requests")
+    time.sleep(1)
+    os.system("pip3 install googletrans")
+    time.sleep(1)
+    os.system("pip3 install thrift")
+    time.sleep(1)
+    os.system("apt install tty-clock")
+    time.sleep(1)
+    os.system("pip3 install bs4")
+    time.sleep(1)
+    os.system("pip3 install html5lib")
+    time.sleep(1)
+    os.system("pip3 install pafy")
+    time.sleep(1)
+    os.system("pip3 install pytz")
+    time.sleep(1)
+    os.system("pip3 install humanfriendly")
+    time.sleep(1)
+    os.system("pip3 install humanize")
+    time.sleep(1)
+    os.system("pip3 install gtts")
+    time.sleep(1)
+    os.system("pip3 install wikipedia")
+    time.sleep(1)
+    os.system("pip3 install youtube_dl")
+    time.sleep(1)
+    os.system("pip3 install antolib")
+    time.sleep(1)
+    os.system("figlet Success")
+    time.sleep(3)
+    restart_program()
+
+elif A == "7" or A == "07":
+    os.system("figlet Loading")
+    time.sleep(2)
+    os.system("figlet Hello!")
+    time.sleep(2)
+    os.system("figlet I am ")
+    time.sleep(2)
+    os.system("figlet EDIT!")
+    time.sleep(2)
+    os.system("figlet Is Ai")
+    time.sleep(2)
+    os.system("figlet Created by")
+    time.sleep(2)
+    os.system("figlet Mr. NN")
+    time.sleep(1)
+    os.system("clear")
+    time.sleep(5)
+    os.system("toilet -f mono12 -F gay N OS")
+    os.system("figlet Please enter")
+    time.sleep(2)
+    os.system("figlet the")
+    time.sleep(2)
+    os.system("figlet Command")
+    time.sleep(3)
+    restart_program()       
+    
+elif A == "8" or A == "08":
+    os.system("figlet Loading")
+    time.sleep(2)
+    os.system("figlet MY") 
+    time.sleep(2)
+    os.system("figlet Creator")
+    time.sleep(2)   
+    print "========================"
+    print " []CREATOR : BOT.             "
+    print " []BY: NN                             "
+    print " []SELFBOT LINE               "
+    print " []JAVA BOT LINE              "
+    print " []                                                    "
+    print " []LINE ID : 0969245004    "
+    print " []                                                    "
+    print "======================== "
+    print
+    time.sleep(5)
+    os.system("toilet -f mono12 -F gay N OS")
+    time.sleep(10)
+    restart_program()
+
+elif A == "001" or A == "0001":
+    os.system("figlet Loading")
+    time.sleep(3)
+    os.system("figlet Install Now")
+    time.sleep(2)
+    os.system("cd && cd storage && cd downloads")
+    time.sleep(1)
+    os.system("git clone https://github.com/nm33/vk")
+    time.sleep(1)
+    os.system("cd vk && unzip Slef3.3.zip")
+    time.sleep(1)
+    os.system("cd Self3.3 && python3 Nlogin.py")
+    
+elif A == "Bot" or A == "bot":
+    os.system("figlet Loading..")
+    time.sleep(2)
+    os.system("figlet Login Now")
+    time.sleep(1)
+    os.system("cd Self3.3 && python3 Nlogin.py")    
+    
+elif A == "002" or A == "0002":
+    os.system("figlet Loading")
+    time.sleep(2)
+    os.system("figlet LOGIN")
+    time.sleep(2)  
+    print
+    print
+    print "class Config(object):"
+    print "LINE_HOST_DOMAIN            = 'https://gd2.line.naver.jp'"
+    print "LINE_OBS_DOMAIN             = 'https://obs-sg.line-apps.com'"
+    print "LINE_TIMELINE_API           = 'https://gd2.line.naver.jp/mh/api'"
+    print "LINE_TIMELINE_MH            = 'https://gd2.line.naver.jp/mh'"
+    print
+    print "LINE_LOGIN_QUERY_PATH       = '/api/v4p/rs'"
+    print "LINE_AUTH_QUERY_PATH        = '/api/v4/TalkService.do'"
+    print
+    print "LINE_API_QUERY_PATH_FIR     = '/S4'"
+    print "LINE_POLL_QUERY_PATH_FIR    = '/P4'"
+    print "LINE_CALL_QUERY_PATH        = '/V4'"
+    print "LINE_CERTIFICATE_PATH       = '/Q'"
+    print "LINE_CHAN_QUERY_PATH        = '/CH4'"
+    print "LINE_SQUARE_QUERY_PATH      = '/SQS1'"
+    print "LINE_SHOP_QUERY_PATH        = '/SHOP4'"
+    print "LINE_LIFF_QUERY_PATH        = '/LIFF1' "
+    print
+    print "'CHANNEL_ID = {"
+    print "'LINE_TIMELINE': '1341209850',"
+    print "'LINE_WEBTOON': '1401600689',"
+    print "'LINE_TODAY': '1518712866',"
+    print "'LINE_STORE': '1376922440',"
+    print "'LINE_MUSIC': '1381425814',"
+    print "'LINE_SERVICES': '1459630796'"
+    print "}"
+    print
+    print "APP_VERSION = {"
+    print "  'ANDROID': '8.14.2',"
+    print  "  'IOS': '8.14.2',"
+    print "  'ANDROIDLITE': '2.1.0',"
+    print "  'BIZANDROID': '1.7.2',"
+    print "  'BIZIOS': '1.7.5',"
+    print "  'BIZWEB': '1.0.22',"
+    print "  'DESKTOPWIN': '5.11.1',"
+    print "  'DESKTOPMAC': '5.11.1',"
+    print "  'IOSIPAD': '8.14.2',"
+    print "  'CHROMEOS': '2.1.5',"
+    print "  'WIN10': '5.5.5',"
+    print "  'CLOVAFRIENDS': '5.5.1',"
+    print "  'DEFAULT': '8.11.0'"
+    print "}"
+    print
+    print
+    print "APP_TYPE    = '   OS   '"
+    print "APP_VER     = APP_VERSION[APP_TYPE] if APP_TYPE in APP_VERSION else APP_VERSION['DEFAULT']"
+    print "CARRIER     = '51089, 1-0'"
+    print "SYSTEM_NAME = 'LOGIN BY. NN'"
+    print "SYSTEM_VER  = '11.2.5'"
+    print "IP_ADDR     = '8.8.8.8'"
+    print "EMAIL_REGEX = re.compile(r'[^@]+@[^@]+\.[^@]+')"
+    print
+    print "def __init__(self, appType=None):"
+    print "   if appType:"
+    print "       self.APP_TYPE = appType"
+    print "       self.APP_VER = self.APP_VERSION[self.APP_TYPE] if self.APP_TYPE in self.APP_VERSION else self.APP_VERSION['DEFAULT']"
+    print "   self.APP_NAME = '%s\t%s\t%s\t%s' % (self.APP_TYPE, self.APP_VER, self.SYSTEM_NAME, self.SYSTEM_VER)"
+    print "   self.USER_AGENT = 'Line/%s' % self.APP_VER"
+    time.sleep(1)
+    os.system("neofetch")
+    time.sleep(1.5)
+    print
+    print
+    os.system("figlet Success")
+    time.sleep(2)
+    print
+    os.system("figlet Restat")
+    time.sleep(2)
+    restart_program()    
+
+elif A == "003" or A == "003":
+    os.system("figlet Loading")
+    time.sleep(2)
+    os.system("toilet -f mono12 -F gay EDIT")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay BY")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay NN")
+    time.sleep(2)
+    os.system("toilet -f mono12 -F gay EDIT")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay BY")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay NN")
+    time.sleep(2)
+    os.system("toilet -f mono12 -F gay EDIT")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay BY")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay NN")
+    time.sleep(2)
+    os.system("toilet -f mono12 -F gay EDIT")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay BY")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay NN")
+    restart_program()
+
+elif A == "nn" or A == "NN":  
+    os.system("clear")
+    time.sleep(1)
+    os.system("cowsay Hello!")
+    os.system("figlet Hello!!!")
+    time.sleep(1)
+    os.system("figlet My name is")
+    time.sleep(1)
+    os.system("figlet NN!!")
+    time.sleep(1)
+    os.system("clear") 
+    os.system("figlet YOU")
+    time.sleep(1)
+    os.system("figlet HAVE")
+    time.sleep(1)
+    os.system("figlet BEEN")
+    time.sleep(1)
+    os.system("figlet HACKED")
+    time.sleep(2)
+    os.system("clear")
+    os.system("figlet 3")
+    time.sleep(1.9)
+    os.system("clear")
+    os.system("figlet 2")
+    time.sleep(1.9)
+    os.system("clear")
+    os.system("figlet 1")
+    time.sleep(1.9)
+    os.system("toilet -f mono12 -F gay NN")
+    time.sleep(1.9)
+    os.system("neofetch")
+    time.sleep(5)
+    os.system("figlet ERROR!!!")
+    time.sleep(5)
+    os.system("cmatrix")
+    
+elif A == "new" or A == "New":
+    os.system("figlet Loading")
+    time.sleep(2)
+    os.system("figlet Login Now")
+    time.sleep(1)
+    os.system("cd new3.3 && python3 newfeary.h")
+    
+elif A == "p" or A == "pp":
+    os.system("figlet Loading")
+    time.sleep(2)
+    os.system("figlet Login Now")
+    time.sleep(1)
+    os.system("cd p && python3 nm33.h")  
+    
+elif A == "porsor" or A == "Porsor":
+    os.system("figlet Loading")
+    time.sleep(2)
+    os.system("figlet Login Now")
+    time.sleep(1)
+    os.system("cd 3000 && python3 prosor.py")  
+
+elif A == "edit" or A == "Edit":
+    os.system("figlet Hello!")
+    time.sleep(2)
+    os.system("toilet I AM EDIT")
+    time.sleep(1)  
+    os.system("toilet -f mono12 -F gay AI")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay BOT")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay LINE")
+    time.sleep(1)
+    os.system("figlet Hello!")
+    time.sleep(2)
+    os.system("toilet I AM EDIT")
+    time.sleep(1)  
+    os.system("toilet -f mono12 -F gay AI")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay BOT")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay LINE")
+    time.sleep(1)
+    os.system("figlet Hello!")
+    time.sleep(2)
+    os.system("toilet I AM EDIT")
+    time.sleep(1.5)
+    os.system("toilet -f mono12 -F gay N OS")
+    time.sleep(1)  
+    os.system("toilet -f mono12 -F gay AI")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay BOT")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay LINE")
+    time.sleep(1)
+    restart_program()
+    
+elif A == "nm" or A == "moo":
+    os.system("figlet Loading")
+    time.sleep(2)
+    os.system("python2 Cloud.py")  
+    
+elif A == "ai" or A == "reboot":
+    os.system("figlet Loading")
+    time.sleep(2)
+    os.system("toilet -f mono12 -F gay 5")
+    time.sleep(1)
+    os.system("clear")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay 4")
+    time.sleep(1)
+    os.system("clear")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay 3")
+    time.sleep(1)
+    os.system("clear")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay 2")
+    time.sleep(1)
+    os.system("clear")
+    time.sleep(1)
+    os.system("toilet -f mono12 -F gay 1")
+    time.sleep(1)
+    os.system("clear")
+    time.sleep(2)
+    os.system("clear")
+    time.sleep(5)
+    os.system("toilet -f mono12 -F gay N OS")
+    time.sleep(3.3)
+    os.system("python3 login.py")     
+
+elif A == "time" or A == "Time":
+    os.system("figlet Loading")
+    time.sleep(5.5)
+    os.system("toilet -f mono12 -F gay N OS")
+    time.sleep(3.3)
+    os.system("clear")
+    os.system("tty-clock") 
+   
+elif A == "youtube" or A == "youtube":
+    os.system("figlet Loading")
+    time.sleep(1)
+    print
+    print
+    os.system("toilet -f mono12 -F gay Open") 
+    time.sleep(2)
+    print 
+    os.system("toilet -f mono12 -F gay YouTube")  
+    time.sleep(3)
+    restart_program()
+    
+elif A == "os" or A == "Os":
+    os.system("figlet Loading")
+    time.sleep(3)
+    os.system("python2 os.py")  
+    
+elif A == "0" or A == "00":
+    sys.exit()
+    
+else:
+     print "\nERROR:Type Number"
+     os.system("figlet ERROR!!")
+     timeout(5)
+     restart_program()
